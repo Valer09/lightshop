118cf3df882fdaeee966abe6dfe979728c148ad6
frenzz <comixgang@gmail.com>
2018-10-11 17:06:01 +0200

    add model and file migration: news, elementShowRoom, Offers

---
+++ database/migrations/2018_10_11_144605_news_migration.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class NewsMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('News', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->timestamp('startDate');
+            $table->timestamp('stopDate');
+            $table->string('pathPhoto');
+            $table->string('linkBuy');
+
+            $table->engine = 'MyISAM';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('News');
+    }
+}
