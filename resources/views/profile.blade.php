0447275c88cf83f8558368b54aea0685aa9f7494
frenzz <comixgang@gmail.com>
2019-03-11 12:28:16 +0100

    dati personali e indirizzi in profilo riempiti

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -34,86 +34,167 @@ function get_el($id){
             <!--lista ordini-->
             <div class="w3-container">
                 <ul class="w3-ul w3-card-4">
-                    <li class="w3-bar">
-                        <span onclick="this.parentElement.style.display='none'" class="w3-bar-item w3-button w3-white w3-xlarge w3-right">×</span>
-                        <div class="w3-bar-item">
-                            <span class="w3-large">SPEDITI</span><br><br>
-                            {{!$orders= get_order()}}
 
-                            @foreach($orders as $order)
-                                @if ($order->order_shipped==1)
+                    <!--CARD PER GLI ORDINI IN CORSO DI ELABORAZIONE-->
+                    {{!$orders= get_order()}}
+                    @foreach($orders as $order)
+                        @if ($order->order_shipped==2)
+                        <li class="w3-bar">
+                            <span onclick="this.parentElement.style.display='none'" class="w3-bar-item w3-button w3-white w3-xlarge w3-right">×</span>
+                            <div class="w3-bar-item">
+                                <span class="w3-large">Effettuato il: {{ $order->added_on }} - <b>In elaborazione</b></span><br><br>
+                                <span>Numero ordine: {{ $order->id }}</span><br>
+                                <span>Oggetti:</span><br>
 
-                                    <span>Ordine N.: {{ $order->id }}</span><br>
-                                    <span>Oggetti:</span><br>
+                                {{!$elements=get_el($order->id)}}
+                                @foreach($elements as $element)
+                                    <span>{{$element->element_name}}</span>Q.ta: {{$element->quantity}}<br>
+                                @endforeach
 
-                                    {{!$elements=get_el($order->id)}}
+                            </div>
+                        </li>
+                        @endif
+                    @endforeach
 
-                                    @foreach($elements as $element)
-                                        <span>{{$element->element_name}}   </span>Q.ta: {{$element->quantity}}<br>
+                    <!--CARD PER GLI ORDINI SPEDITI-->
+                    {{!$orders= get_order()}}
+                    @foreach($orders as $order)
+                        @if ($order->order_shipped==1)
+                        <li class="w3-bar">
+                            <span onclick="this.parentElement.style.display='none'" class="w3-bar-item w3-button w3-white w3-xlarge w3-right">×</span>
+                            <div class="w3-bar-item">
+                                <span class="w3-large">Effettuato il: {{ $order->added_on }} - <b>Spedito</b></span><br><br>
+                                <span>Numero ordine: {{ $order->id }}</span><br>
+                                <span>Oggetti:</span><br>
 
-                                    @endforeach
-                                @endif
-                            @endforeach
+                                {{!$elements=get_el($order->id)}}
+                                @foreach($elements as $element)
+                                    <span>{{$element->element_name}}</span>Q.ta: {{$element->quantity}}<br>
+                                @endforeach
 
-                        </div>
-                    </li>
+                            </div>
+                        </li>
+                        @endif
+                    @endforeach
 
-                    <li class="w3-bar">
-                        <span onclick="this.parentElement.style.display='none'" class="w3-bar-item w3-button w3-white w3-xlarge w3-right">×</span>
-                        <div class="w3-bar-item">
-                            <span class="w3-large">IN CORSO</span><br><br>
-                            {{!$orders= get_order()}}
+                    <!--CARD PER GLI ORDINI COMPLETATI-->
+                    {{!$orders= get_order()}}
+                    @foreach($orders as $order)
+                        @if ($order->order_shipped==3)
+                        <li class="w3-bar">
+                            <span onclick="this.parentElement.style.display='none'" class="w3-bar-item w3-button w3-white w3-xlarge w3-right">×</span>
+                            <div class="w3-bar-item">
+                                <span class="w3-large">Effettuato il: {{ $order->added_on }} - <b>Completato</b></span><br><br>
+                                <span>Numero ordine: {{ $order->id }}</span><br>
+                                <span>Oggetti:</span><br>
 
-                            @foreach($orders as $order)
-                                @if ($order->order_shipped==2)
+                                {{!$elements=get_el($order->id)}}
+                                @foreach($elements as $element)
+                                    <span>{{$element->element_name}}</span>Q.ta: {{$element->quantity}}<br>
+                                @endforeach
 
-                                    <span>Ordine N.: {{ $order->id }}</span> <br>
-
-                                    {{!$elements=get_el($order->id)}}
-                                    <span>Oggetti:</span><br>
-
-                                    @foreach($elements as $element)
-                                        <span>{{$element->element_name}}</span> Q.ta: {{$element->quantity}}<br>
-                                    @endforeach
-                                @endif
-                            @endforeach
-
-                        </div>
-                    </li>
-
-                    <li class="w3-bar">
-                        <span onclick="this.parentElement.style.display='none'" class="w3-bar-item w3-button w3-white w3-xlarge w3-right">×</span>
-                        <div class="w3-bar-item">
-                                <span class="w3-large">COMPLETATI</span><br><br>
-                            {{!$orders= get_order()}}
-
-                            @foreach($orders as $order)
-                                @if ($order->order_shipped==3)
-                                    <span>Ordine N.: {{ $order->id }}</span><br>
-                                        {{!$elements=get_el($order->id)}}
-                                        <span>Oggetti:</span><br>
-
-                                            @foreach($elements as $element)
-                                                <span>{{$element->element_name}}</span> Q.ta: {{$element->quantity}}<br>
-                                            @endforeach
-
-                                @endif
-                            @endforeach
-
-                        </div>
-                    </li>
+                            </div>
+                        </li>
+                        @endif
+                    @endforeach
                 </ul>
             </div>
         </div>
 
         <div id="dati" class="w3-container w3-border city" style="display:none">
             <h2>Dati personali</h2>
-            <p>Modifica i tuoi dati di pagamento.</p>
+
+            <div class="w3-padding">
+                <div class="w3-row">
+                    <label>Nome e cognome: <b>Mario Rotolo</b></label>
+                </div>
+                <div class="w3-row">
+                    <label>Data di nascita: <b>23/2/1789</b></label>
+                </div>
+                <div class="w3-row">
+                    <label>Codice fiscale: <b>FWER45ERT345RTeR</b></label>
+                </div>
+                <div class="w3-row">
+                    <label>Partita IVA: <b>12345678</b></label>
+                </div>
+                <div class="w3-row">
+                    <label>E-mail: <b>mario.rotolo@live.it</b></label>
+                </div>
+                <div class="w3-row">
+                    <label>PEC: <b>mariorotolo@pec.it</b></label>
+                </div>
+            </div>
+            <button onclick="openModal('modificaDati')">Modifica dati</button>
+
+            <div id="modificaDati" class="w3-modal">
+                <div class="w3-modal-content w3-animate-top w3-card-4">
+                    <header class="w3-container w3-teal">
+                        <span onclick="document.getElementById('modificaDati').style.display='none'" class="w3-button w3-display-topright">&times;</span>
+                        <h2>Modifica i tuoi dati.</h2>
+                    </header>
+                    <div class="w3-padding">
+                        <div class="w3-row">
+                            <label>Nome e cognome:</label>
+                            <input placeholder="Nome">
+                            <input placeholder="Cognome">
+                        </div>
+                        <div class="w3-row">
+                            <label>Data di nascita:</label>
+                            <input type="date" value="1-1-1990">
+                        </div>
+                        <div class="w3-row">
+                            <label>Codice fiscale:</label>
+                            <input placeholder="Codice fiscale">
+                        </div>
+                        <div class="w3-row">
+                            <label>Partita IVA:</label>
+                            <input placeholder="Partita IVA">
+                        </div>
+                        <div class="w3-row">
+                            <label>E-mail:</label>
+                            <input placeholder="E-mail">
+                        </div>
+                        <div class="w3-row">
+                            <label>PEC: </label>
+                            <input placeholder="PEC">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div id="spedizione" class="w3-container w3-border city" style="display:none">
             <h2>Indirizzi di spedizione</h2>
-            <p>Gestisci i tuoi indirizzi di spedizione.</p>
+            <div class="w3-container">
+                <ul class="w3-ul w3-card-4">
+                    <!--CARD PER GLI INDIRIZZI DI SPEDIZIONE-->
+                    <li class="w3-bar">
+                        <span onclick="this.parentElement.style.display='none'" class="w3-bar-item w3-button w3-white w3-xlarge w3-right">×</span>
+                        <div class="w3-bar-item">
+                            <span class="w3-middle"><b>Mario Rotolo</b></span><br>
+                            <span>
+                                Via Collepretara, 432
+                                <br>L'aquila, italy 67100
+                                <br>tel. 2345435345
+                                <br>Note: interno A
+                            </span>   
+                        </div>
+                    </li>
+
+                    <li class="w3-bar">
+                        <span onclick="this.parentElement.style.display='none'" class="w3-bar-item w3-button w3-white w3-xlarge w3-right">×</span>
+                        <div class="w3-bar-item">
+                            <span class="w3-middle"><b>Federico Rotolo</b></span><br>
+                            <span>
+                                Via valle, 4
+                                <br>L'aquila, italy 67100
+                                <br>tel. 2876543
+                                <br>Note: scala 3
+                            </span>   
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
 
         <div id="pagamento" class="w3-container w3-border city" style="display:none">
