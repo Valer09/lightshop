a98b6e98b4aadb71e37f86ad545863989ca70a72
frenzz <comixgang@gmail.com>
2019-03-13 10:58:45 +0100

    aggiornamento showroom, da finire

--- resources/views/showroom.blade.php
+++ resources/views/showroom.blade.php
@@ -4,6 +4,52 @@
 @section('content')
 
     <!-- PAGE CONTENT -->
+<div id="pageContent" class="w3-display-container w3-teal">
+    <div id="principalDiv" class="w3-row">
+    </div>
+    <div id="boxShow" class="w3-row w3-bottom">
+        <div class="w3-col imgshow l2">
+            <figure>
+                <img class="imagShow" src="./images/ceramiche/ceramiche (5).jpg" alt="Pavimenti e Rivestimenti">
+                <figcaption>Pavimenti e Rivestimenti</figcaption>
+            </figure>
+        </div>
+        <div class="w3-col imgshow l2">
+            <figure>
+                <img class="imagShow" src="./images/cucine/cucina (1).jpg" alt="Cucine">
+                <figcaption>Cucine</figcaption>
+            </figure>
+        </div>
+        <div class="w3-col imgshow l2">
+            <figure>
+                <img class="imagShow" src="./images/bagno/bagno (2).jpg" alt="Bagni">
+                <figcaption>Bagni</figcaption>
+            </figure>
+        </div>
+        <div class="w3-col imgshow l2">
+            <figure>
+                <img class="imagShow" src="./images/ferramenta/ferramenta (6).jpg" alt="Porte">
+                <figcaption>Porte</figcaption>
+            </figure>
+        </div>
+        <div class="w3-col imgshow l2">
+            <figure>
+                <img class="imagShow" src="./images/caminetti/caminetto (2).jpg" alt="Caminetti">
+                <figcaption>Caminetti</figcaption>
+            </figure>
+        </div>
+        <div class="w3-col imgshow l2">
+            <figure>
+                <img class="imagShow" src="./images/porticati/porticati (1).jpg" alt="Falegnameria">
+                <figcaption>Falegnameria</figcaption>
+            </figure>
+        </div>
+    </div>
+</div>
+   
+
+
+    <!--
 <div id="principalDiv" class="w3-display-container w3-animate-opacity w3-text-white">
     <div  class="w3-row w3-display-right">
         <section id="container">
@@ -45,7 +91,7 @@
             </div>
         </section>
     </div>
-</div>
+</div>-->
     <!-- End PAGE CONTENT -->
 
 @stop
