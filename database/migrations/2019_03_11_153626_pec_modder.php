24ce62acffbaf2353d7da67790fffa597479db17
Vale <valerio.marchitelli@student.univaq.it>
2019-03-13 16:10:43 +0100

    fix bullone

--- database/migrations/2019_03_11_153626_pec_modder.php
+++ database/migrations/2019_03_11_153626_pec_modder.php
@@ -14,8 +14,8 @@ class PecModder extends Migration
     public function up()
     {
         Schema::table('users', function($table) {
-            $table->string('CF')->default('A');
-            $table->string('IVA')->default('0');
+          //  $table->string('CF')->default('A');
+         //   $table->string('IVA')->default('0');
             //$table->string('PEC')->default('nopec@pec.it');
         });
     }
@@ -28,8 +28,8 @@ class PecModder extends Migration
     public function down()
     {
         Schema::table('users', function($table) {
-            $table->dropColumn('C.F');
-            $table->dropColumn('P.IVA');
+       //     $table->dropColumn('C.F');
+       //     $table->dropColumn('P.IVA');
             //$table->dropColumn('PEC');
         });
     }
