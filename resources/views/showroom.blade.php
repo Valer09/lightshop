5fc341624fe39f9f54cf22da0511fcfaef2462d3
frenzz <comixgang@gmail.com>
2019-03-14 12:20:30 +0100

    showroom finito

--- resources/views/showroom.blade.php
+++ resources/views/showroom.blade.php
@@ -5,39 +5,44 @@
 
     <!-- PAGE CONTENT -->
 
-    <header class="w3-display-container w3-wide" id="principalDiv">
-        <div class="w3-display-topmiddle w3-margin-top w3-margin">
-            <h1 class="w3-xxlarge w3-center w3-text-white"><span class="w3-padding w3-green w3-opacity-min"><b>V</b></span> <span class="w3-hide-small w3-text-light-grey">Showroom</span></h1>
-        </div>
-    </header>
 
-    <div id="boxShow" class="w3-row w3-white w3-container">
-        <div id="show1" class="show w3-col l2  w3-hover-opacity">
-            <div class="w3-third w3-right w3-white w3-opacity-min" style="height:200px"><span class="testoShow">Pavimenti e Rivestimenti</span></div>
-        </div>
 
-        <div id="show2" class="show w3-col l2 w3-hover-opacity">
-            <div class="w3-third w3-right w3-white w3-opacity-min" style="height:200px"><span class="testoShow">Cucine</span></div>
-        </div>
- 
-        <div id="show3" class="show w3-col l2 w3-hover-opacity">
-            <div class="w3-third w3-right w3-white w3-opacity-min" style="height:200px"><span class="testoShow">Bagni</span></div>
-        </div>
-
-        <div id="show4" class="show w3-col l2 w3-hover-opacity">
-            <div class="w3-third w3-right w3-white w3-opacity-min" style="height:200px"><span class="testoShow">Porte</span></div>
-        </div>
-
-        <div id="show5" class="show w3-col l2 w3-hover-opacity">
-            <div class="w3-third w3-right w3-white w3-opacity-min" style="height:200px"><span class="testoShow">Caminetti</span></div>
-        </div>
-
-        <div id="show6" class="show w3-col l2 w3-hover-opacity">
-            <div class="w3-third w3-right w3-white w3-opacity-min" style="height:200px"><span class="testoShow">Falegnameria</span></div>
-        </div>
+<header class="w3-display-container w3-wide" id="principalDiv">
+    <div class="w3-display-topmiddle w3-margin-top w3-margin">
+        <h1 class="w3-xxlarge w3-center w3-text-white"><span class="w3-padding w3-green w3-opacity-min"><b>V</b></span> <span class="w3-hide-small w3-text-light-grey">Showroom</span></h1>
+    </div>
+</header>
+<div id="boxShow" class="w3-row w3-white w3-container">
+    <div id="show1" class="show w3-col l2">
+        <div class="testoShow w3-right w3-white w3-opacity-min">
+        <span>Pavimenti e Rivestimenti</span></div>
     </div>
 
-   
+    <div id="show2" class="show w3-col l2">
+        <div class="testoShow w3-right w3-white w3-opacity-min">
+        <span>Cucine</span></div>
+    </div>
+
+    <div id="show3" class="show w3-col l2">
+        <div class="testoShow w3-right w3-white w3-opacity-min">
+        <span>Bagni</span></div>
+    </div>
+
+    <div id="show4" class="show w3-col l2">
+        <div class="testoShow w3-right w3-white w3-opacity-min">
+        <span>Porte</span></div>
+    </div>
+
+    <div id="show5" class="show w3-col l2">
+        <div class="testoShow w3-right w3-white w3-opacity-min">
+        <span>Caminetti</span></div>
+    </div>
+
+    <div id="show6" class="show w3-col l2">
+        <div class="testoShow w3-right w3-white w3-opacity-min">
+        <span>Falegnameria</span></div>
+    </div>
+</div>
 
 
     <!--
