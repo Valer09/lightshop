0d4b134e9eff4a2f69dc742374abbb2173291ec2
frenzz <comixgang@gmail.com>
2019-03-15 15:30:20 +0100

    carrello quasi completato

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -7,28 +7,51 @@
         <!--ELEmento carrello-->
         <div class="w3-card">
             <div class="w3-black w3-center"><h2>CARRELLO</h2></div>
+        </div>
 
-        </div>
         <div class="w3-card">
-            <div class="w3-padding">
-                <img style="height: 120px" src="https://images.pexels.com/photos/1036936/pexels-photo-1036936.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+            <!--ELEMENTO 1 DEL CARRELLO-->
+            <div class="w3-padding w3-row">
+                <div class="fotoCarrello w3-container w3-quarter" style="max-width: 150px">
+                    <img style="max-width: 150px" src="https://images.pexels.com/photos/1036936/pexels-photo-1036936.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+                </div> 
+                <div class="w3-container w3-rest">
+                    <h5><b>Lamapde da muro fluorescenti</b></h5>
+                    <P>lamapadine gialle a incandescenza</P>
+                    <P>Quantità: 53 pz</P>
+                </div>
             </div>
+            <!--fine ELEMENTO 1 DEL CARRELLO-->
+
+            <!--ELEMENTO 2 DEL CARRELLO-->
+            <div class="w3-padding w3-row">
+                <div class="fotoCarrello w3-container w3-quarter" style="max-width: 150px">
+                    <img style="max-width: 150px" src="https://images.pexels.com/photos/1036936/pexels-photo-1036936.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+                </div> 
+                <div class="w3-container w3-rest">
+                    <h5><b>Lamapde da muro fluorescenti</b></h5>
+                    <P>lamapadine gialle a incandescenza</P>
+                    <P>Quantità: 53 pz</P>
+                </div>
+            </div>
+            <!--fine ELEMENTO 2 DEL CARRELLO-->
         </div>
+        
     </div>
     <!--TOTALE-->
     <div class="w3-container w3-third w3-black">   
     <h1>Riepilogo</h1>
         <div class="boxTotale w3-row">
-            <span class="w3-left">Subtotale:</span>
-            <span class="w3-right">190.23 €</span>
+            <span class="tot w3-left">Subtotale:</span>
+            <span class="tot w3-right">190.23 €</span>
         </div>
         <div class="boxTotale w3-row">
-            <span class="w3-left">Costi di spedizione e gestione:</span>
-            <span class="w3-right">12.50 €</span>
+            <span class="tot w3-left">Costi di spedizione e gestione:</span>
+            <span class="tot w3-right">12.50 €</span>
         </div>
         <div class="boxTotale w3-row">
-            <span class="w3-left"><b>TOTALE</b></span>
-            <span class="w3-right"><b>202.73 €</b></span>
+            <span class="tot w3-left"><b>TOTALE</b></span>
+            <span class="tot w3-right"><b>202.73 €</b></span>
         </div>
     </div>
 </div><!-- End PAGE CONTENT -->
