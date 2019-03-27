6e1df2f07afb82264620f357e86eb309955bddcd
frenzz <comixgang@gmail.com>
2019-03-27 15:09:12 +0100

    Modale per inserimetno sottocategorie funz.

--- public/js/backend.js
+++ public/js/backend.js
@@ -1,5 +1,5 @@
 // Used to toggle the menu on small screens when clicking on the menu button
-function soffietto(id) {
+function soffietto(id, idButton) {
     var x = document.getElementById(id);
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
@@ -8,6 +8,13 @@ function soffietto(id) {
         x.className = x.className.replace("w3-show", "");
         x.previousElementSibling.className =
             x.previousElementSibling.className.replace(" w3-theme-d1", "");
+    }
+
+    var x = document.getElementById(idButton);
+    if (x.className.indexOf("w3-green") == -1) {
+        x.className += " w3-green";
+    } else {
+        x.className = x.className.replace("w3-green", "");
     }
 }
 
@@ -23,4 +30,12 @@ function openModal(idModale, idProdotto, nome, descr, category) {
 //close modal
 function closeModal(idModale){
     document.getElementById(idModale).style.display='none';
+}
+
+function modaleSottocategoria(idModale, categoria) {
+    document.getElementsByClassName('inputModale').value = '';
+    document.getElementById('modaleCategorySub').value = categoria.toString();
+    document.getElementById(idModale).style.display='block';
+    document.getElementById('categoriaLabel').innerText = "  in: "+ categoria;
+
 }
--- resources/views/backAdmin/categories.blade.php
+++ resources/views/backAdmin/categories.blade.php
@@ -14,22 +14,18 @@
                 {{!$Category=\App\Category::all()}}
                 {{!$Subcategorylist=\App\Subcategory::all()}}
                 @foreach($Category as $Category)
-                    <button onclick="soffietto('{{$Category->name}}')" class="w3-button w3-block w3-theme-l1 w3-left-align">
+                    <button id="button{{$Category->name}}" onclick="soffietto('{{$Category->name}}', this.id);" class="w3-button w3-block w3-theme-l1 w3-left-align">
                         <i class="fa fa-circle-o-notch fa-fw w3-margin-right"></i>
                         {{$Category->name}}
                     </button>
 
-                    <div id="{{$Category->name}}" class="w3-hide w3-container">
-                        
-                            <div id="cardCategorie">
-                                @foreach($Subcategorylist as $Subcategory)
-                                @if ($Subcategory->category == $Category->name)
-                                    <p>{{ $Subcategory->name }}</p>
-                                @endif
-                                @endforeach
-                                <p><a>Aggiungi nuova sottoclasse</a></p>
-                            </div>
-
+                    <div id="{{$Category->name}}" class="cardCategorie w3-hide w3-container w3-light-grey"> 
+                        @foreach($Subcategorylist as $Subcategory)
+                        @if ($Subcategory->category == $Category->name)
+                            <p>{{ $Subcategory->name }}</p>
+                        @endif
+                        @endforeach
+                        <p><button onclick="modaleSottocategoria('nuovaSottoclasse', '{{$Category->name}}')" class="w3-button"><i class="fa fa-plus"></i> Aggiungi nuova sottoclasse</button></p>
                     </div>
                 @endforeach
  
@@ -37,5 +33,29 @@
         </div>
     </div>
 </div>
+
+<!--Modale Nuovo indirizzo-->
+<div id="nuovaSottoclasse" class="w3-modal">
+            <div class="w3-modal-content w3-animate-top w3-card-4" style="max-width: 700px">
+                <header class="w3-container w3-teal">
+                    <span onclick="closeModal('nuovaSottoclasse');" class="w3-button w3-display-topright">&times;</span>
+                    <h2>Nuova sottoclasse</h2>
+                </header>
+                <form type="submit" method="post" action="{{URL::to('/subcategory_insertion_submit')}}?ref={{$_SERVER['REQUEST_URI']}}">
+                    @csrf
+                    <div class="w3-padding">
+                        <div class="w3-row">
+                            <label>Sottocategoria: </label>
+                            <input class="inputModale" placeholder="Sottocategoria" type="text" name="name" required>
+                            <input type="text" class="w3-hide" id="modaleCategorySub" name="category" required>
+                            <label id="categoriaLabel"></label>
+                        </div>
+                        <div class="w3-row">
+                            <button class="w3-right" type="submit">Salva</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
 
 @stop
