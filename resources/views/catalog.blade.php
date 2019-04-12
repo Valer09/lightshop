46b70997808e6e71978ad05ef10d3dfdc8839c80
frenzz <comixgang@gmail.com>
2019-04-12 09:10:47 +0200

    Catalogo da riallineare

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -53,10 +53,6 @@
         @else
         <p class="w3-left">{{ $Category[0]->name }}</p>
         @endif
-        <p class="w3-right">
-            <i class="fa fa-shopping-cart w3-margin-right"></i>
-            <i class="fa fa-search"></i>
-        </p>
     </header>
 
     <!-- Image header -->
@@ -93,7 +89,9 @@
                                     <i class="fa fa-shopping-cart"></i></button>
                             </div>
                         </div>
+                        <div class="divElP">
                         <p>{{ $el->name }}<br><b>€ {{ number_format($el->price, 2, ',', '.') }}</b></p>
+                        </div>
                     </div>
                     {{!$conta = $conta + 1}}
 
@@ -103,101 +101,5 @@
             @endif
         @endforeach
     </div>
-<!--ESEMPIOPOO
-        <div class="w3-col l3 s6">
-            <div class="w3-container">
-                <div class="w3-display-container">
-                    <img src="{{asset('storage')}}/images/catalogo/bullone.jpg" style="width:100%">
-                    <div class="w3-display-middle w3-display-hover">
-                        <button onclick="window.location.href='./prodotto.html'" class="w3-button w3-black">Acquista ora
-                            <i class="fa fa-shopping-cart"></i></button>
-                    </div>
-                </div>
-                <p>Ripped Skinny Jeans<br><b>$24.99</b></p>
-            </div>
-            <div class="w3-container">
-                <div class="w3-display-container">
-                    <img src="./images/catalogo/bullone.jpg" style="width:100%">
-                    <div class="w3-display-middle w3-display-hover">
-                        <button onclick="window.location.href='./prodotto.html'" class="w3-button w3-black">Acquista ora
-                            <i class="fa fa-shopping-cart"></i></button>
-                    </div>
-                </div>
-                <p>Mega Ripped Jeans<br><b>$19.99</b></p>
-            </div>
-        </div>
-
-        <div class="w3-col l3 s6">
-            <div class="w3-container">
-                <div class="w3-display-container">
-                    <img src="./images/catalogo/bullone.jpg" style="width:100%">
-                    <span class="w3-tag w3-display-topleft">New</span>
-                    <div class="w3-display-middle w3-display-hover">
-                        <button onclick="window.location.href='./prodotto.html'" class="w3-button w3-black">Acquista ora
-                            <i class="fa fa-shopping-cart"></i></button>
-                    </div>
-                </div>
-                <p>Mega Ripped Jeans<br><b>$19.99</b></p>
-            </div>
-            <div class="w3-container">
-                <div class="w3-display-container">
-                    <img src="./images/catalogo/bullone.jpg" style="width:100%">
-                    <span class="w3-tag w3-display-topleft">New</span>
-                    <div class="w3-display-middle w3-display-hover">
-                        <button onclick="window.location.href='./prodotto.html'" class="w3-button w3-black">Acquista ora
-                            <i class="fa fa-shopping-cart"></i></button>
-                    </div>
-                </div>
-                <p>Washed Skinny Jeans<br><b>$20.50</b></p>
-            </div>
-        </div>
-
-        <div class="w3-col l3 s6">
-            <div class="w3-container">
-                <div class="w3-display-container">
-                    <img src="./images/catalogo/bullone.jpg" style="width:100%">
-                    <div class="w3-display-middle w3-display-hover">
-                        <button onclick="window.location.href='./prodotto.html'" class="w3-button w3-black">Acquista ora
-                            <i class="fa fa-shopping-cart"></i></button>
-                    </div>
-                </div>
-                <p>Washed Skinny Jeans<br><b>$20.50</b></p>
-            </div>
-            <div class="w3-container">
-                <div class="w3-display-container">
-                    <img src="./images/catalogo/bullone.jpg" style="width:100%">
-                    <span class="w3-tag w3-display-topleft">Sale</span>
-                    <div class="w3-display-middle w3-display-hover">
-                        <button class="w3-button w3-black">Acquista ora <i class="fa fa-shopping-cart"></i></button>
-                    </div>
-                </div>
-                <p>Vintage Skinny Jeans<br><b class="w3-text-red">$14.99</b></p>
-            </div>
-        </div>
-
-        <div class="w3-col l3 s6">
-            <div class="w3-container">
-                <div class="w3-display-container">
-                    <img src="./images/catalogo/bullone.jpg" style="width:100%">
-                    <span class="w3-tag w3-display-topleft">New</span>
-                    <div class="w3-display-middle w3-display-hover">
-                        <button onclick="window.location.href='./prodotto.html'" class="w3-button w3-black">Acquista ora
-                            <i class="fa fa-shopping-cart"></i></button>
-                    </div>
-                </div>
-                <p>Vintage Skinny Jeans<br><b>$14.99</b></p>
-            </div>
-            <div class="w3-container">
-                <div class="w3-display-container">
-                    <img src="./images/catalogo/bullone.jpg" style="width:100%">
-                    <div class="w3-display-middle w3-display-hover">
-                        <button onclick="window.location.href='./prodotto.html'" class="w3-button w3-black">Acquista ora
-                            <i class="fa fa-shopping-cart"></i></button>
-                    </div>
-                </div>
-                <p>Ripped Skinny Jeans<br><b>$24.99</b></p>
-            </div>
-        </div>
--->
-    </div>
+</div>
 @stop
