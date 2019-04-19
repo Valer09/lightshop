52c4d508ca9e3a3a1087707b72cabbd065ca305e
frenzz <comixgang@gmail.com>
2019-04-19 12:21:17 +0200

    errore in js checkout

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
     $(".radioCourier").change(function () {
         var costoSped = parseFloat(this.classList[2]);
         var subTotale = parseFloat($('#subtotale').text());
-        console.log(subTotale);
         $("#costoSped").text(costoSped.toFixed(2));
         $("#totaleCheckout").text((costoSped + subTotale).toFixed(2));
     });
