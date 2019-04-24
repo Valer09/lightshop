582fb91811334799c979458c703fd0f3c469c2bb
frenzz <comixgang@gmail.com>
2019-04-24 09:05:00 +0200

    migration offerts

---
+++ database/migrations/2019_04_24_085950_mod_offerts.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModOfferts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('offerts', function (Blueprint $table) {
+            $table->integer('id_element')->unsigned();
+            $table->string('description')->nullable()->default(null)->change();
+            $table->string('pathPhoto')->nullable()->default(null)->change();
+            $table->string('linkBuy')->nullable()->default(null)->change();
+            $table->foreign('id_element')->references('id')->on('elements')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('offerts', function (Blueprint $table) {
+            //
+        });
+    }
+}
