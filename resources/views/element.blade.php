647de855118bb7921289992cc33bfb411d55fb45
frenzzz <comixgang@gmail.com>
2019-07-06 22:17:59 +0200

    Spec in element

--- resources/views/element.blade.php
+++ resources/views/element.blade.php
@@ -136,70 +136,17 @@
 						<div class="table-responsive">
 							<table class="table">
 								<tbody>
+									{{!$specs = App\SpecElement::where('id_element', $el->id)->get()}}
+									@foreach ($specs as $spec)
 									<tr>
 										<td>
-											<h5>Width</h5>
+											<h5>{{ $spec->key_spec }}</h5>
 										</td>
 										<td>
-											<h5>128mm</h5>
+											<h5>{{ $spec->value_spec }}</h5>
 										</td>
 									</tr>
-									<tr>
-										<td>
-											<h5>Height</h5>
-										</td>
-										<td>
-											<h5>508mm</h5>
-										</td>
-									</tr>
-									<tr>
-										<td>
-											<h5>Depth</h5>
-										</td>
-										<td>
-											<h5>85mm</h5>
-										</td>
-									</tr>
-									<tr>
-										<td>
-											<h5>Weight</h5>
-										</td>
-										<td>
-											<h5>52gm</h5>
-										</td>
-									</tr>
-									<tr>
-										<td>
-											<h5>Quality checking</h5>
-										</td>
-										<td>
-											<h5>yes</h5>
-										</td>
-									</tr>
-									<tr>
-										<td>
-											<h5>Freshness Duration</h5>
-										</td>
-										<td>
-											<h5>03days</h5>
-										</td>
-									</tr>
-									<tr>
-										<td>
-											<h5>When packeting</h5>
-										</td>
-										<td>
-											<h5>Without touch of hand</h5>
-										</td>
-									</tr>
-									<tr>
-										<td>
-											<h5>Each Box contains</h5>
-										</td>
-										<td>
-											<h5>60pcs</h5>
-										</td>
-									</tr>
+									@endforeach
 								</tbody>
 							</table>
 						</div>
