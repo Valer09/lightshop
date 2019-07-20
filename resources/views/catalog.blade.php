2c5deb623124a92388ab4079c3169d75cdb20ab4
frenzzz <comixgang@gmail.com>
2019-07-20 20:05:29 +0200

    Prezzo sconto catalogo

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -157,10 +157,15 @@
                             </div>
                             <div class="item-price">
                               <div class="price-box">
+                                @if(isset($Offerts[$el->id]) && $Offerts[$el->id]->date_end > date('Y-m-d h:i:sa'))
                                 <p class="old-price"><span class="price-label">Regular Price:</span> <span
-                                    class="price">$100.00 </span> </p>
+                                    class="price">€{{ number_format($el->price, 2, ',', '.') }} </span> </p>
                                 <p class="special-price"><span class="price-label">Special Price</span> <span
-                                    class="price">{{ $el->price }} </span> </p>
+                                    class="price" style="color: red">€{{ number_format(($el->price - (($el->price)/100*$Offerts[$el->id]->discount_perc)), 2, '.', ',') }} </span> </p>
+                                @else
+                                <p class="special-price"><span class="price-label">Regular Price:</span> <span
+                                    class="price">€{{ number_format($el->price, 2, ',', '.') }} </span> </p>
+                                @endif
                               </div>
                             </div>
                             <div class="action">
